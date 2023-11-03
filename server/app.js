0d45e313caccb5ab317c9e0317d775f48718fe32
Oleg Bravo <oleg.bravo1853@gmail.com>
2023-11-03 14:46:22 +0200

    add dotenv and cors

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
 
 module.exports = app;
