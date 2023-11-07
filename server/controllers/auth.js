d61e4026c13bc9344f8faf4b8ce5ade26876bcf0
Oleg Bravo <oleg.bravo1853@gmail.com>
2023-11-07 14:14:50 +0200

    add auto login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,11 +5,23 @@ async function loginByForm(req, res) {
   const { login, password } = req.body;
   const user = await findUser(login, password);
   if (user) {
-    const token = jwt.sign({ login, password }, 'secret', { expiresIn: '24h' });
-    res.json({ user, token })
+    const token = jwt.sign({ login, password }, "secret", { expiresIn: "24h" });
+    res.json({ user, token });
   } else {
-    res.json({ error: 'Неверно указан логин или пароль' });
+    res.json({ error: "Неверно указан логин или пароль" });
   }
 }
 
-module.exports = { loginByForm };
+async function autoLogin(req, res) {
+  const { token } = req.body;
+  try {
+    const parsedToken = jwt.verify(token, "secret");
+    const { login, password } = parsedToken;
+    const user = await findUser(login, password);
+    const newToken = jwt.sign({ login, password }, "secret", { expiresIn: "24h" });
+    res.json({ user, token: newToken });
+  } catch {
+    res.json({ error: "Токен устарел" });
+  }
+}
+module.exports = { loginByForm, autoLogin };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { loginByForm } = require('../controllers/auth')
+const { loginByForm, autoLogin } = require('../controllers/auth')
 
 router.post("/registration", (req, res) => {
   res.send("Регистрация");
 });
 
-router.post("/login", (req, res) => {
-  res.send("Логин при заходе на сайт");
-});
+router.post("/login", autoLogin);
 
 router.post("/login-form", loginByForm);
 
